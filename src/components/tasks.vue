c9e20392bf9107d4e5b7654eefb7be0f7081ec82
dga80 <dani.dga@gmail.com>
2022-12-15 12:30:27 +0100

    add timeTask

--- src/components/tasks.vue
+++ src/components/tasks.vue
@@ -4,7 +4,7 @@
       <input type="text" placeholder="título" v-model="title" class="" />
       <button type="submit">CREATE</button>
     </form>
- 
+
     <table class="table-auto mx-auto border-solid bg-yellow">
       <thead>
         <tr>
@@ -12,51 +12,57 @@
           <th>TODO</th>
           <th>In Progress</th>
           <th>COMPLETED</th>
-          <th>Tasks data </th>
+          <th>Tasks data</th>
         </tr>
       </thead>
 
-        <tbody>
-          <tr>
-            <th>  {{ userStore.user.email }} </th> 
+      <tbody>
+        <tr>
+          <th>{{ userStore.user.email }}</th>
 
-            <th>
-              <div v-for="(task) in tasksStore.tasks">
-                <div v-if="task.status == 1" >
-                  {{ task.title}}              
-                  <form @submit.prevent="tasksStore.updateTask(2, task.id)">
-                   <button >Hacer</button>
-                  </form> 
-                 </div>
-              </div>             
-            </th>
-
-            <th><div v-for="(task) in tasksStore.tasks">   
-                  <div v-if="task.status == 2" >
-                  {{ task.title}}
-                    <form @submit.prevent="updateCurrent(3, task.id)">
-                      <button >Hecha</button>
-                    </form>
-                  </div>
+          <th>
+            <div v-for="task in tasksStore.tasks">
+              <div v-if="task.status == 1">
+                {{ task.title }}
+                <div class="text-xs">
+                  {{ task.inserted_at.slice(0,10)}}
+                  {{ task.inserted_at.slice(11,19)}}
                 </div>
-            </th>
-
-            <th>
-              <div v-for="(task) in tasksStore.tasks">   
-                <div v-if="task.status == 3" >
-                {{ task.title}} 
-                <!-- <form @submit.prevent="borrar(task.id), tasksStore.fetchTasks()">                             -->
-                 <button @click="borrar(task.id)" >DELETE</button>
-                 <!-- </form>    -->
-                </div>
+                  <button @click="updateCurrent(2, task.id)">TO DO</button>
               </div>
-            </th>
+            </div>
+          </th>
 
-            <th> {{ tasksStore.tasks }}</th>
-          </tr>
-        </tbody>
+          <th>
+            <div v-for="task in tasksStore.tasks">
+              <div v-if="task.status == 2">
+                {{ task.title }}
+                <div class="text-xs">
+                  {{ task.inserted_at.slice(0,10)}}
+                  {{ task.inserted_at.slice(11,19)}}
+                </div>
+                  <button @click="updateCurrent(3, task.id)">DONE</button>
+              </div>
+            </div>
+          </th>
 
-      </table>  
+          <th>
+            <div v-for="task in tasksStore.tasks">
+              <div v-if="task.status == 3">
+                {{ task.title }}
+                <div class="text-xs">
+                  {{ task.inserted_at.slice(0,10)}}
+                  {{ task.inserted_at.slice(11,19)}}
+                </div>
+                <button @click="borrar(task.id)">DELETE</button>
+              </div>
+            </div>
+          </th>
+
+          <th>{{ tasksStore.tasks }}</th>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -71,50 +77,51 @@ export default {
     return {
       user_id: null,
       title: null,
-      status: "1"
+      status: "1",
     };
   },
   computed: {
     ...mapStores(userStore, tasksStore),
-    },
+  },
 
   methods: {
-
-   async createNew() {
+    async createNew() {
       await this.tasksStore.createTask(
         this.userStore.user.id,
         this.title,
-        this.status
-      );  
-      await this.tasksStore.fetchTasks()  
-     },
+        this.status,
+        this.timeTask
+      );
+      await this.tasksStore.fetchTasks();
+    },
 
-      async updateCurrent(status, id) {
-      await this.tasksStore.updateTask(status, id); 
-        await this.tasksStore.fetchTasks();  
-      },     
-   
-     async borrar(id) {
+    async updateCurrent(status, id) {
+      await this.tasksStore.updateTask(status, id);
+      await this.tasksStore.fetchTasks();
+    },
+
+    async borrar(id) {
       await this.tasksStore.deleteTask(id);
-      await this.tasksStore.fetchTasks();  
-    }
-  }, 
+      await this.tasksStore.fetchTasks();
+    },
+  },
 
-   mounted(){
-       this.tasksStore.fetchTasks()
- },
- };
-  </script>
-  
-  
-  <style scoped>
-  input{
-    border: 1px solid;
-  }
-   table, th, td {
+  mounted() {
+    this.tasksStore.fetchTasks();
+  },
+};
+</script>
+
+<style scoped>
+input {
   border: 1px solid;
 }
-button{
+table,
+th,
+td {
+  border: 1px solid;
+}
+button {
   border: 1px solid;
 }
 </style>
