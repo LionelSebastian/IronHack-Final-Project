95fe2cabe208b8170ff4e7b8c1f6d9c402a81f51
LionelSebastian <lionel.sebastian.cifo@gmail.com>
2022-12-15 17:21:07 +0100

    Fixing => Correcion width sidebar to 100% inestead of 100vh

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <body class=" flex min-h-screen">
+    <body class=" flex min-h-full">
         <div class=" flex-col bg-blue-100 py-6 px-3"> 
             <RouterLink to="/dashboard/user"  >usuario</RouterLink>
             <img src="../../public/Ironhack_logologo.png" alt="">
