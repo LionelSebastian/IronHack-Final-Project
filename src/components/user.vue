0b53e96f73e9ca769dc34cbaf49710d21999f550
dga80 <dani.dga@gmail.com>
2022-12-19 19:10:45 +0100

    last

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,29 +1,25 @@
+<template>
+  <div class="text-center mx-auto">
+    <h1 class="py-6">Hello {{ userStore.user.first_name }}</h1>
+    <h3 class="py-6">E-mail: {{ userStore.user.email }}</h3>
+    <h3 class="py-6">Phone number: {{ userStore.user.phone }}</h3>
+    <h3 class="py-6">You are registred in: {{}}</h3>
+    <h3 class="py-6">
+      Has archivado XX tareas(podemos agregar un status apra archivadas)
+    </h3>
+  </div>
+</template>
 
-  <template>
-    <div class="text-center mx-auto">
-     <h1 class="py-6">Hello {{ userStore.user.first_name}}</h1>  
-     <h3 class="py-6">E-mail: {{ userStore.user.email}}</h3>
-     <h3 class="py-6">Phone number: {{ userStore.user.phone}}</h3>
-     <h3 class="py-6">You are registred in: {{}}</h3>
-     <h3 class="py-6">Has archivado XX tareas(podemos agregar un status apra archivadas)</h3>
-    </div>
-  </template>
-  
-  <script>
-  import { mapStores } from "pinia"
-  import userStore from '../stores/user'
-  import tasksStore from '../stores/tasks'
-  
-  export default{
-    computed:{
-      ...mapStores(userStore, tasksStore)      
-     },
-  }
-  
-  </script>  
-  
-  <style scoped>
-  
-  
-  </style>
-  
+<script>
+import { mapStores } from "pinia";
+import userStore from "../stores/user";
+import tasksStore from "../stores/tasks";
+
+export default {
+  computed: {
+    ...mapStores(userStore, tasksStore),
+  },
+};
+</script>
+
+<style scoped></style>
