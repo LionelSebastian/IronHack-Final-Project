cb9fa0b81e839716718b13a990d541f8d6e03d2c
Ignacio Silva <ignaciosilvadeleon@gmail.com>
2022-12-19 13:54:09 +0100

    few: auth

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,51 +1,44 @@
 <template>
-    <navbar />
-  
-    <body class="text-center bg-slate-400">  
-    
-    <RouterView/>
-        
-    <!-- <RouterLink to="/Dashboard/tasks">Go to Dashboard</RouterLink> -->
-    </body>
-    <foot />
-
+  <navbar />
+  <body class="text-center bg-slate-400">
+    <RouterView />
+  </body>
+  <foot />
 </template>
-    
+
 <script>
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterLink, RouterView } from "vue-router";
 import { mapStores } from "pinia";
-import userStore from '../stores/user';
-import navbar from '../components/navbar.vue';
-import foot from '../components/foot.vue';
-import SignIn from '../components/SignIn.vue' 
+import userStore from "../stores/user";
+import navbar from "../components/navbar.vue";
+import foot from "../components/foot.vue";
+import SignIn from "../components/SignIn.vue";
 
-export default{
-    components: {navbar, foot, SignIn},
-    computed:{
-      ...mapStores(userStore)
-     },
-     data(){
-        return {
-            user: null,
-            password: null,
-        }
-     },
-    methods:{
-     login(){
-        this.userStore.signIn(this.user, this.password);
-     }   
+export default {
+  components: { navbar, foot, SignIn },
+  computed: {
+    ...mapStores(userStore),
+  },
+  data() {
+    return {
+      user: null,
+      password: null,
+    };
+  },
+  methods: {
+    login() {
+      this.userStore.signIn(this.user, this.password);
     },
-}
+  },
+};
 </script>
-    
-    
-<style scoped>
-    input{
-        border: solid 2px black
-    }
-    button{
-        border: solid 2px black;
-        margin: 5px;
-    }
-</style>
 
+<style scoped>
+input {
+  border: solid 2px black;
+}
+button {
+  border: solid 2px black;
+  margin: 5px;
+}
+</style>
