411321a215a1fa35300317c3c9467016acb45202
Ignacio Silva <ignaciosilvadeleon@gmail.com>
2022-12-21 14:17:50 +0100

    feat: navBar in sideBar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,13 +1,17 @@
 <template>
-   <!-- <div :class="`flex py-2 px-5 justify-between bg-slate-500`" > -->
-    <!-- <img src="../icons/LNDteam2.png" class="h-7" alt="Logo"> -->
-    <div class="flex gap-4 py-2 px-5 justify-between bg-slate-300">
-        <h1 class="text-sky-900">{{ userStore.user.user_metadata.name }}</h1>
-        <button v-if="userStore.user !== null" @click="logOut()" class="cursor-pointer" alt="Sign Out"><img class="h-5" src="../../public/logout-icon.png" alt="Sign Out"></button>
-    </div>
-   <!-- </div> -->
+  <div class="flex gap-4 m-5 justify-between bg-slate-300">
+    <h1 class="text-sky-900">{{ userStore.user.user_metadata.name }}</h1>
+    <button
+      v-if="userStore.user !== null"
+      @click="logOut()"
+      class="cursor-pointer"
+      alt="Sign Out"
+    >
+      <img class="h-5" src="../../public/logoutBlue.png" alt="Sign Out" />
+    </button>
+  </div>
 </template>
-   
+
 <script>
 import { mapStores } from "pinia";
 import userStore from "../stores/user";
@@ -16,29 +20,18 @@ export default {
   computed: {
     ...mapStores(userStore),
   },
-  data(){
+  data() {
     return {
       flexStart: "justify-start",
       spaceBetween: "justify-between",
-    }
+    };
   },
   methods: {
     logOut() {
       this.userStore.signOut();
     },
-    // justify() {
-    //   if (this.userStore.user !== null) {
-    //     return this.spaceBetween;
-    //   }
-    //   return this.flexStart
-    // }
   },
 };
 </script>
-   
-   
-<style scoped>
 
-   
-</style>
-   
+<style scoped></style>
